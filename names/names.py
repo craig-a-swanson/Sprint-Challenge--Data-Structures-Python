b8cefe33440da4f529b0717dfa71691e2f277252
Craig Swanson <cswanson02@me.com>
2020-09-28 10:35:30 -0500

    Added stretch goal for names challenge

--- names/names.py
+++ names/names.py
@@ -20,7 +20,7 @@ duplicates = []  # Return the list of duplicates in this data structure
 #             duplicates.append(name_1)
 
 # The idea is to put all of the names in the first list into an alphabetized binary tree.
-# Once that three is created, look through the array of names in the second list and 
+# Once that tree is created, look through the array of names in the second list and 
 # compare each one to the existing tree.
 # If the tree contains that name, add it to the duplicates list.
 name1_tree = bst_node_class.BSTNode(names_1[0])
@@ -39,3 +39,21 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+start_time2 = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates2 = []
+
+duplicates2 = set(names_1).intersection(names_2)
+
+end_time2 = time.time()
+print (f"{len(duplicates2)} duplicates:\n\n{', '.join(duplicates2)}\n\n")
+print (f"runtime: {end_time2 - start_time2} seconds")
